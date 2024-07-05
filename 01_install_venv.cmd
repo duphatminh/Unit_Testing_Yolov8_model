2389867f444d4259bb55547d189a44f11b7df8b3
DESKTOP-F7B8ATK <123456minhko@gmail.com>
2024-07-05 17:25:28 +0700

    Update batch venv and run_unittest

--- 01_install_venv.cmd
+++ 01_install_venv.cmd
@@ -6,10 +6,5 @@ call .venv\Scripts\activate
 python.exe -m pip install --upgrade pip
 
 REM pip install -r requirements.txt
-pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121
 
 pip install ultralytics
-pip install -U openmim
-mim install mmengine
-mim install "mmcv==2.1.0"
-mim install mmdet
---
+++ 03_run_unittest.cmd
@@ -0,0 +1,1 @@
+call python -m unittest test.YOLO8s.py
